b9e160fc4e82b1b978117d01d312324e3ec5657e
shubaivan <shuba.ivan@yandex.ru>
2021-09-08 14:39:25 +0300

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/sh -l
 
-echo "init token $1 TARNAME $TARNAME DIRNAME $DIRNAME"
-apiOutput=$(curl -i --request "DELETE" -d "path=/$DIRNAME/$TARNAME" -H "Content-type: application/x-www-form-urlencoded" \
+token=$1
+filename=$2
+qbee_directory=$3
+local_directory=$4
+
+echo "init token $token filename $filename qbee_directory $qbee_directory and local_directory $local_directory"
+
+apiOutput=$(curl -i --request "DELETE" -d "path=/$qbee_directory/$filename" -H "Content-type: application/x-www-form-urlencoded" \
    --url 'https://www.app.qbee.io:9443/api/v2/file'\
    --header 'Authorization: Bearer '"$1")
 echo "API output is:\n$apiOutput"
 
-apiOutput=$(curl -i --request POST -H "Content-Type:multipart/form-data" -F "path=/$DIRNAME/" -F "file=@./tar/$TARNAME" \
+apiOutput=$(curl -i --request POST -H "Content-Type:multipart/form-data" -F "path=/$qbee_directory/" -F "file=@./$local_directory/$filename" \
    --url 'https://www.app.qbee.io:9443/api/v2/file'\
    --header 'Authorization: Bearer '"$1")
 echo "API output is:\n$apiOutput"
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=token::$1"
+echo "::set-output name=token::$token"
