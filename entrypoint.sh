a81a81bdcd462b154ff37c5fc03a5ffb0b8baf7e
shubaivan <shuba.ivan@yandex.ru>
2021-09-09 15:57:13 +0300

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,16 @@ successful_status_code='200'
 
 echo "init token $token filename $filename qbee_directory $qbee_directory and local_directory $local_directory"
 
-apiOutput=$(curl -i --request "DELETE" -sL -d "path=/$qbee_directory/$filename" -H "Content-type: application/x-www-form-urlencoded" \
+apiOutput=$(curl --request "DELETE" -sL -d "path=/$qbee_directory/$filename" -H "Content-type: application/x-www-form-urlencoded" \
    --url 'https://www.app.qbee.io:9443/api/v2/file'\
-   --header 'Authorization: Bearer '"$1"\
+   --header 'Authorization: Bearer '"$token"\
    -w "\n{\"http_code\":%{http_code}}\n")
 
-echo $apiOutput
-echo 'DELETE request '
-http_code=$(echo "$apiOutput" | jq -cs | jq -r '.[1].http_code')
+echo 'DELETE request'
+http_code=$(echo $apiOutput | jq -cs | jq -r '.[1].http_code')
 echo $http_code
 
-if [ "$http_code" != "$successful_status_code" ]
+if [ "$http_code" != "$successful_status_code"] &&  ["$http_code" != "400"];
 
 then
     echo "http_code was - $http_code"
@@ -30,9 +29,9 @@ fi
 
 echo "API output is:\n$apiOutput"
 
-apiOutput=$(curl -i --request POST -H "Content-Type:multipart/form-data" -F "path=/$qbee_directory/" -F "file=@$local_directory$filename" \
+apiOutput=$(curl --request POST -sL -H "Content-Type:multipart/form-data" -F "path=/$qbee_directory/" -F "file=@$local_directory$filename" \
    --url 'https://www.app.qbee.io:9443/api/v2/file'\
-   --header 'Authorization: Bearer '"$1"\
+   --header 'Authorization: Bearer '"$token"\
    -w "\n{\"http_code\":%{http_code}}\n")
 
 echo 'POST request'
