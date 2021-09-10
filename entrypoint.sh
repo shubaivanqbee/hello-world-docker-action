ff6503a2155ddabecdb40b708b482ae2ed2db100
thomas-lud <72799533+thomas-lud@users.noreply.github.com>
2021-09-10 13:38:04 +0200

    update entrypoint.sh
    * change echo information
    * remove token output
    * modified $local_directory to obtain a correct path definition
    * modified $qbee_directory to include leading and trailing slash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,62 +1,67 @@
 #!/bin/bash -l
 
-
-echo $SHELL
-
-bash --version
-
+#inputs 
 token=$1
 filename=$2
 qbee_directory=$3
 local_directory=$4
+
 successful_status_code='200'
 
 echo "init token $token filename $filename qbee_directory $qbee_directory and local_directory $local_directory"
 
-apiOutput=$(curl --request "DELETE" -sL -d "path=/$qbee_directory/$filename" -H "Content-type: application/x-www-form-urlencoded" \
-   --url 'https://www.app.qbee.io:9443/api/v2/file'\
-   --header 'Authorization: Bearer '"$token"\
-   -w "\n{\"http_code\":%{http_code}}\n")
-echo $apiOutput
+#get absolute path and add trailing slash
+local_directory="$(cd $local_directory && pwd)/"
+
+#make sure qbee_directory has both leading and trailing slash
+[[ "${qbee_directory}" != /* ]] && qbee_directory="/${qbee_directory}"
+[[ "${qbee_directory}" != */ ]] && qbee_directory="${qbee_directory}/"
+
+apiOutput=$(curl --request "DELETE" -sL -d "path=$qbee_directory$filename" \
+            -H "Content-type: application/x-www-form-urlencoded" \
+            --url 'https://www.app.qbee.io:9443/api/v2/file' \
+            --header 'Authorization: Bearer '"$token" \
+            -w "\n{\"http_code\":%{http_code}}\n")
+
+
 echo 'DELETE request'
 http_code=$(echo $apiOutput | jq -cs | jq -r '.[1].http_code')
-echo $http_code
+
+echo "API output is:"
+echo $apiOutput
 
 if [ "$http_code" == "null" ]; then
     http_code=$(echo $apiOutput | jq -cs | jq -r '.[0].http_code')
 else
-    echo "http_code was not null"
+    #echo "http_code was not null"
 fi
 
 if [[ "$http_code" != "$successful_status_code" && "$http_code" != "400" && "$http_code" != "204" ]]; then
     echo "http_code was - $http_code"
+    echo "something went wrong ... aborting"
     exit 1
 else
     echo "http_code was - $http_code"
 fi
 
-echo "API output is:\n$apiOutput"
-
-apiPostOutput=$(curl --request POST -sL -H "Content-Type:multipart/form-data" -F "path=/$qbee_directory/" -F "file=@$local_directory$filename" \
-   --url 'https://www.app.qbee.io:9443/api/v2/file'\
-   --header 'Authorization: Bearer '"$token"\
-   -w "\n{\"http_code\":%{http_code}}\n")
+apiPostOutput=$(curl --request POST -sL -H "Content-Type:multipart/form-data" \
+               -F "path=/$qbee_directory/" -F "file=@$local_directory$filename" \
+               --url 'https://www.app.qbee.io:9443/api/v2/file'\
+               --header 'Authorization: Bearer '"$token"\
+               -w "\n{\"http_code\":%{http_code}}\n")
 
 echo 'POST request'
-echo $apiPostOutput
 post_http_code=$(echo $apiPostOutput | jq -cs | jq -r '.[1].http_code')
-echo $post_http_code
+
+echo "API Post output is:"
+echo $apiPostOutput
 
 if [ "$post_http_code" != "$successful_status_code" ]
 
 then
     echo "http_code was - $post_http_code"
+    echo "something went wrong ... aborting"
     exit 1
-:
 else
     echo "http_code was - $post_http_code"
 fi
-
-echo "API Post output is:\n$apiPostOutput"
-
-echo "::set-output name=token::$token"
